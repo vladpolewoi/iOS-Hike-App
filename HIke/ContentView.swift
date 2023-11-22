83d9bd5fc7222be9d098669a9390b9225c85dcad
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-11-22 18:30:10 +0200

    Initial Commit

---
+++ HIke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HIke
+//
+//  Created by Quest76 on 22.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HIke/HIkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIkeApp.swift
+//  HIke
+//
+//  Created by Quest76 on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HIkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
